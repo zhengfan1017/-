d33f36073c4e8b2fd7e427eee6b852d195a1319f
zhengfan1017 <zhengfan1017@users.noreply.github.com>
2026-05-11 13:21:17 +0000

    Revert commits 92ad62c..92ad62c
    This reverts 1 commit(s) from 92ad62c to 92ad62c.
    
    Reverted commits:
    92ad62c feat: 编写ROS2节点
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- ros2_ws/compile_and_run.sh
+++
@@ -1,1 +0,0 @@
-g++ -o hello_five_meat /workspace/ros2_ws/src/hello_five_meat/src/simple_node.cpp && /workspace/ros2_ws/hello_five_meat
--- ros2_ws/src/hello_five_meat/src/simple_node.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "哈喽，五花肉" << std::endl;
-    return 0;
-}
